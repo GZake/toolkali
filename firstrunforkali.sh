5c4ec5a92585de8ae23b2aaf674fe452415925cc
GZake <48483210+GZake@users.noreply.github.com>
2022-06-02 09:13:28 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -1,6 +1,8 @@
 sudo apt-get -f -y install parallel* #status=progress
 sudo apt-get -f -y install parallel
 sudo parallel cp -vfxR firstrunforkali* ::: ~ /media/kali/*/ /media/*/*/
+cd ..
+sudo parallel cp -vfxR toolkali/ ::: ~ /media/kali/*/ /media/*/*/
 cd ~
 #!/bin/bash
 # include this boilerplate
