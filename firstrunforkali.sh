883ac177060a8e7ca4afdacb238b9abeab20104f
GZake <48483210+GZake@users.noreply.github.com>
2022-06-04 12:36:11 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -478,6 +478,66 @@ tar --wildcards --no-anchored --warning=none -xf /icu-61.1-1-x86_64.pkg.tar.xz *
 sudo cp /usr/lib/libicu*.so.61* /usr/lib
 sudo apt-get install -f -y ibus-unikey*
 sudo apt-get install -f -y ibus-unikey
+sudo apt --fix-broken install -y | tee  $HOME/Desktop/log.txt
+sudo apt update --fix-missing -y | tee -a  $HOME/Desktop/log.txt
+sudo dpkg --configure -a | tee -a  $HOME/Desktop/log.txt
+sudo apt-get install -f -y snap
+sudo apt-get install -f -y snap*
+sudo apt-get install -f -y *snap*
+sudo apt-get install -f -y snapd
+sudo apt-get install -f -y snapd*
+apparmor_parser -r /var/lib/snapd/apparmor/profiles/*
+sudo apt-get install -f -y synaptic*
+sudo systemctl enable snapd.socket snapd apparmor
+sudo systemctl start snapd.socket snapd apparmor
+sudo snap install snap-store
+apparmor_parser -r /var/lib/snapd/apparmor/profiles/*
+sudo apt-get install -f -y gnome-software
+sudo apt-get install -f -y gnome-software*
+sudo apt-get install -f -y software-center
+sudo apt-get install -f -y software-center*
+sudo apt-get install -f -y *spotify*
+sudo apt-get install -f -y spotify-client
+sudo apt-get install -f -y *brave-browser*
+sudo apt-get install -f -y brave-browser-nightly
+sudo apt-get install -f -y *grub-efi*
+sudo apt-get install -f -y grub-efi*
+sudo apt-get install -f -y grub-efi
+sudo apt-get install -f -y packaging-dev sbuild apt-file gitk git-lfs myrepos
+sudo apt-get install -f -y flameshot*
+sudo apt-get install -f -y dnf*
+sudo apt-get install -f -y kali-root-login*
+sudo apt-get install -f -y flameshot 
+sudo apt-get install -f -y dnf
+sudo apt-get install -f -y alsa-base alsa-tools alsa-tools-gui alsa-utils alsa-oss alsamixergui libalsaplayer0
+sudo apt-get install -f -y kmix
+sudo apt-get install -f -y kmix*
+sudo apt-get install -f -y pulseaudio
+sudo apt-get install -f -y pulseaudio*
+sudo apt-get install -f -y gnome-core
+sudo apt-get install -f -y gnome-core*
+sudo apt-get install -f -y gnome-tweak
+sudo apt-get install -f -y gnome-tweak*
+sudo apt-get install -f -y gnome-tweak-tool
+sudo apt-get install -f -y gnome-tweak-tool*
+sudo apt-get install -f -y xserver-xorg-core-dbg
+sudo apt-get install -f -y xserver-xorg*
+sudo apt-get install -f -y debian-goodies
+sudo apt-get install -f -y debian-goodies*
+sudo apt-get install -f -y gnupg2 curl
+sudo apt-get install -f -y gnupg2 curl*
+sudo apt-get install -f -y gnupg*
+sudo apt-get install -f -y anydesk
+sudo apt-get install -f -y anydesk*
+sudo apt-get install -f -y libc++
+wget https://discord.com/api/download?platform=linux&format=deb
+sudo dpkg -i download?platform=linux&format=deb
+find-dbgsym-packages [core_path|running_pid|binary_path]
+curl -Lo /icu-61.1-1-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/i/icu/icu-61.1-1-x86_64.pkg.tar.xz
+tar --wildcards --no-anchored --warning=none -xf /icu-61.1-1-x86_64.pkg.tar.xz *libicudata.so.61* *libicui18n.so.61* *libicuuc.so.61*
+sudo cp /usr/lib/libicu*.so.61* /usr/lib
+sudo apt-get install -f -y ibus-unikey*
+sudo apt-get install -f -y ibus-unikey
 sudo bash install.sh
 #sudo bash update.sh
 sudo timedatectl set-timezone Asia/Ho_Chi_Minh
