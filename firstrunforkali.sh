9766811f45cf532769ecae626daf407f71acfe34
GZake <48483210+GZake@users.noreply.github.com>
2022-06-28 10:21:48 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -514,7 +514,7 @@ echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sou
 sudo apt-add-repository http://deb.playonlinux.com/
 sudo apt-add-repository http://archive.mariadb.org/mariadb-10.5.9/repo/ubuntu/
 sudo apt-add-repository http://apt.postgresql.org/pub/repos/apt/
-sudo apt-add-repository http://http.kali.org/kali/
+#sudo apt-add-repository http://http.kali.org/kali/
 sudo apt-add-repository https://packages.microsoft.com/
 sudo apt-add-repository http://dl.winehq.org/wine-builds/ubuntu/
 sudo apt-add-repository http://deb.debian.org/debian/
