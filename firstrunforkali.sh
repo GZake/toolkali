b384798963210d03112098e4a62148f9cb59aa31
SUGirlBae <88968281+SUGirlBae@users.noreply.github.com>
2022-07-15 09:49:46 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -92,10 +92,10 @@ sudo systemctl daemon-reload
 ulimit -Sn unlimited
 ulimit -n unlimited
 ulimit -c unlimited
-ulimit -u unlimited
 ulimit -i unlimited
 ulimit -l unlimited
 ulimit -s unlimited
+ulimit -u unlimited -n 65536
 sysctl -w fs.file-max=2147483647
 fs.file-max = 2147483647
 sudo apt-get  -o dir::cache='/var/cache/apt/archives/' -o Debug::NoLocking=1 install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --show-progress --install-suggests *xargs*
