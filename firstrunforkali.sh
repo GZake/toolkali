2eb828d1cab4d53d896953cea6463c5db073f353
SUGirlBae <88968281+SUGirlBae@users.noreply.github.com>
2022-07-16 17:04:40 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -1034,9 +1034,9 @@ sudo cp -fRv /usr/share/keyrings/* /etc/apt/trusted.gpg.d/
 sudo cp -fRv /etc/apt/trusted.gpg.d/* /usr/share/keyrings/
 sudo cp -fRv /usr/share/keyrings/* $HOME/toolkali/
 cd ~/toolkali
-git remote add origin https://github.com/GZake/toolkali.git
-git branch -M main
-git push -u origin main
+#git remote add origin https://github.com/GZake/toolkali.git
+#git branch -M main
+#git push -u origin main
 
 if [ $choose1 -eq 4 ]
 then
