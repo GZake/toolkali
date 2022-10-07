f178ba94f6e560aae99b1d500e9bb0d2f140300c
SUGirlBae <88968281+SUGirlBae@users.noreply.github.com>
2022-10-07 10:35:02 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -111,8 +111,9 @@ ulimit -i unlimited
 ulimit -l unlimited
 ulimit -s unlimited
 ulimit -u unlimited -n 1048576
-sysctl -w fs.file-max=2147483647
+sudo sysctl -w fs.file-max=2147483647
 fs.file-max = 2147483647
+sudo sysctl -p
 sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests *xargs*
 apt show PACKAGES 2>&1 | sed -nr '/^(Suggests|Package): /{s///;s/( \|[^,]+)?,//g;p}' | xargs apt -y install
 sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests apt-transport-https
