e5a2162b6923de6e387fab07d67b435129a32517
SUGirlBae <iamnonexist@sugirl.onmicrosoft.com>
2022-10-13 16:09:27 +0700

    1
    1

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -101,7 +101,7 @@ sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fy
 #sudo apt-mark unhold *y*
 #sudo apt-mark unhold *z*
 sudo apt-mark hold wine1.6
-git clone https://github.com/LionSec/katoolin.git
+[ ! -f $HOME/Documents/GitHub/toolkali/ ] || sudo git clone https://github.com/LionSec/katoolin.git $HOME/Documents/GitHub/toolkali/
 cp katoolin/katoolin.py /usr/bin/katoolin
 chmod +x /usr/bin/katoolin
 sudo chmod -Rv 777 /var/cache/apt/archives/ 
