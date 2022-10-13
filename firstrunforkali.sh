6a7535c4ae7b8bb7a8e09848a3be91c5dec7bb62
SUGirlBae <88968281+SUGirlBae@users.noreply.github.com>
2022-10-13 08:45:50 +0000

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 echo $(pwd)
 [[ -f $HOME/Downloads/brave-browser-dev_1.45.96_amd64.deb ]] || sudo wget https://github.com/brave/brave-browser/releases/download/v1.45.96/brave-browser-dev_1.45.96_amd64.deb -P $HOME/Downloads/
-sudo dpkg -i  brave-browser-dev_*
+sudo dpkg -i  $HOME/Downloads/brave-browser-dev_*
 [[ -f $HOME/Downloads/GitHubDesktop-linux-3.0.6-linux1.deb]] || sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.0.6-linux1/GitHubDesktop-linux-3.0.6-linux1.deb -P $HOME/Downloads/
-sudo dpkg -i  GitHubDesktop-linux*
+sudo dpkg -i  $HOME/Downloads/GitHubDesktop-linux*
 choose:
 clear
 sudo timedatectl set-timezone Asia/Ho_Chi_Minh >> $HOME/toolkali/log/log_$(date "+%d_%h_%y").txt
