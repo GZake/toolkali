8a792ee9a0a372e7c96282dba85b9bfe649cb951
SUGirl <88968281+SUGirlBae@users.noreply.github.com>
2022-10-14 13:07:15 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -1560,7 +1560,7 @@ reboot
 gitinstall:
 sudo dpkg -i  $HOME/Downloads/brave-browser-dev_*
 sudo dpkg -i  $HOME/Downloads/GitHubDesktop-linux*
-sudo apt-get reinstall -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests python3
+sudo apt-get reinstall -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests python3*
 goto uptab
 
 
