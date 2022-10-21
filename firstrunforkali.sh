3b709c5eb1494d333fe7c4bf703eb4f2e08aa89f
SUGirl <88968281+SUGirlBae@users.noreply.github.com>
2022-10-21 11:19:01 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -231,9 +231,9 @@ sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fy
 sudo parallel cp -vfxR firstrunforkali* ::: $HOME /media/kali/*/ /media/*/*/
 sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests  linux-headers-$(uname -r)
 sudo apt-get install -o APT::Install-Recommends=1 -o APT::Install-Suggests=1 -fym --ignore-hold --install-recommends --allow-change-held-packages --show-progress --install-suggests  kde-plasma-desktop
-cd $HOME
+#cd $HOME
 sudo parallel cp -vfxR $HOME/toolkali/ ::: $HOME /media/kali/*/ /media/*/*/
-cd $HOME/Documents/GitHub/toolkali/
+#cd $HOME/Documents/GitHub/toolkali/
 sudo swapon /dev/nvm*
 sudo swapon /dev/sd*
 #sudo swapon /dev/nvme0n1p5
@@ -441,8 +441,8 @@ goto choose
 #####################################
 uptool:
 clear
-cd ~
-$(pwd)
+#cd ~
+#$(pwd)
 sudo rm -fRdv $HOME/toolkali 
 sudo rm -fRdv /media/*/toolkali
 sudo rm -fRdv /media/*/*/toolkali 
