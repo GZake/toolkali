bcee631e6b2201bef18823488cafaa8aac0b1e1f
SUGirl <88968281+SUGirlBae@users.noreply.github.com>
2022-10-25 10:35:05 +0700

    Update firstrunforkali.sh

--- firstrunforkali.sh
+++ firstrunforkali.sh
@@ -12,7 +12,7 @@ sudo apt-mark hold apt-avahi-discover
 #measure enemy tattoo retire female nephew creek  horror fade connect connect half expose crime wage document purity horse renew plate glide guard fiction ripple
 #https://download3.vmware.com/software/WS-PUBTP-22H2/VMware-Workstation-Full-e.x.p-20486661.x86_64.bundle
 refresh:
-cd ..
+cd ~
 echo $(pwd)
 [[ -f $HOME/Downloads/brave-browser-dev_1.45.96_amd64.deb ]] || sudo wget https://github.com/brave/brave-browser/releases/download/v1.45.96/brave-browser-dev_1.45.96_amd64.deb -P $HOME/Downloads/
 [[ -f $HOME/Downloads/GitHubDesktop-linux-3.0.6-linux1.deb ]] || sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.0.6-linux1/GitHubDesktop-linux-3.0.6-linux1.deb -P $HOME/Downloads/
